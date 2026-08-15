9b98162f1b2388e7bdb8ea54dcc157da56f1156a
venkatasaikosuru <venkatasaikosuru01@gmail.com>
2026-08-15 23:18:49 +0530

    my first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+console.log("Hello world")
+console.log("Hello world")
+console.log("Hello world")
+try {
+    console.log(a)
+} catch (error) {
+    console.log(error)
+}
+console.log("Hello world1")
+console.log("Hello world2")
